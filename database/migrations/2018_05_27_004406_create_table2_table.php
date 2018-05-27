0347b5936f09ac5852ae9ae089d0befdc5129479
Cuji <facbrizio@gmail.com>
2018-05-27 01:16:56 -0300

    migraciones completadas, para ejecutar ejecutar 'php artisan migrate' estando en el el directorio del proyecto

--- database/migrations/2018_05_27_004406_create_table2_table.php
+++ database/migrations/2018_05_27_004406_create_table2_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTable2Table extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('table2', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('table1Id')->unsigned();
+            $table->foreign('table1Id')->references('id')->on('table1');
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('table2');
     }
 }
