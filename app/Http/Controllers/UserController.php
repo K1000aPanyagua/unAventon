837ed95b082b0bd82b11814f4b00788d2a7dfd95
Cuji <facbrizio@gmail.com>
2018-05-30 01:20:05 -0300

    sadad

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,67 +2,42 @@
 
 namespace App\Http\Controllers;
 
-
-use Validator;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 use Auth;
 use App\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    
-    
 
     public function store(Request $request)
     {
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-=======
->>>>>>> 6f51cdc1843b9d735563aab0fc6a1b832ad67794
-    
->>>>>>> 5f4d9c3aef79b3efac66f84d6cbe5759fe1d7564
+
         //Validation
         $request->validate([
             'name' => 'required|string',
             'lastname' => 'required|string',
             'birthdate' => 'required|date',
             'password' => 'required|string|confirmed',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'telephone' => 'string',
         
         ]);
 
-        if($validator->fails()) {
-            return Redirect::back()->withErrors($validator);
-        } 
 
         //Almacenamiento
         $user = new User;
@@ -77,60 +52,30 @@ class UserController extends Controller
 
         //Redireccion
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-        return view('search');
-}
-=======
-        
 
 
->>>>>>> 6f51cdc1843b9d735563aab0fc6a1b832ad67794
         return view('search');
     }
->>>>>>> 5f4d9c3aef79b3efac66f84d6cbe5759fe1d7564
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show($id)
     {
         $user=User::find($id);
         return view('user.show', compact('user'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function edit($id)
     {
-<<<<<<< HEAD
-        //
-=======
+
         //Carga vista de editar perfil
->>>>>>> 5f4d9c3aef79b3efac66f84d6cbe5759fe1d7564
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
-<<<<<<< HEAD
-        //
-=======
+
         $user->name = $request->name;
         $user->lastname = $request->lastname;
         $user->birthdate = $request->birthdate;
@@ -142,22 +87,12 @@ class UserController extends Controller
 
         //Redireccion
         return view('home');
->>>>>>> 5f4d9c3aef79b3efac66f84d6cbe5759fe1d7564
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {
-<<<<<<< HEAD
-        //
-    }
-}
-=======
         Item::find(1)->delete();
         return view('home')->with([
             'flash_message' => 'Usuario eliminado',
@@ -165,4 +100,7 @@ class UserController extends Controller
             ]);
     }
 }
->>>>>>> 5f4d9c3aef79b3efac66f84d6cbe5759fe1d7564
+
+        
+
+
