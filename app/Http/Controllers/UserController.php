294ad4299c5b04f606d6e94587167f8c2b6ecdd7
cachorra666 <camilapaniaguagaudio@gmail.com>
2018-05-29 21:55:26 -0300

    bla bla

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,12 +37,7 @@ class UserController extends Controller
     public function store(Request $request)
     {
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 2d62591df638f6166f53c8a102a1d1ea75f2a791
-    
         //Validation
         /*$request->validate([
             'name' => 'required|string',
@@ -71,10 +66,7 @@ class UserController extends Controller
 
         return view('search');
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 2d62591df638f6166f53c8a102a1d1ea75f2a791
     }
 
     /**
